7c9eaa1517fc49e7ba224c8ec27a232acc908bb4
Kevin <kevin1999.2050@gmail.com>
2026-06-25 11:47:10 +0530

    Added FrameBuffer Logic

--- BabyDriver/babydriver.c
+++ BabyDriver/babydriver.c
@@ -1,1 +1,24 @@
-//Just checking
+#include "babydriver.h"
+
+int FRAME_BUFFER[FRAME_TOTAL] = {0};
+
+void bit_set_rest( int x , int y, bool state)
+{
+    int pos = (FRAME_WIDTH*y + x) >> 3;
+
+    if(state)
+    {
+        FRAME_BUFFER[pos] |= (0x80 >> (x & 0x07) );
+    }
+    else 
+    {
+         FRAME_BUFFER[pos] &= ~(0x80 >> (x & 0x07) );
+
+    }
+}
+
+bool gt_bit_val(int x, int y)
+{
+    int pos = (FRAME_WIDTH*y + x) >> 3;
+    return FRAME_BUFFER[pos] & (0x80 >> (x & 0x07));
+}
---
+++ BabyDriver/babydriver.h
@@ -0,0 +1,14 @@
+#ifndef BABYDRIVER_H
+#define BABYDRIVER_H
+
+#include <stdbool.h>
+#include <stdio.h>
+
+#define FRAME_WIDTH 255
+#define FRAME_HEIGHT 128 
+
+#define FRAME_TOTAL FRAME_HEIGHT*FRAME_WIDTH /8
+
+
+
+#endif
